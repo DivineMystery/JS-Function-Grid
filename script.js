174473045e380aaec271bb6d9bfb143cb0175ad1
Mustapha <mustapha.o.ojo@student.uncommonhs.org>
2017-03-01 21:59:51 +0000

    harmbe is dead

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //Write your function calls below this line
+setColor(0,0,"blue");
+setColor(0,9,"blue");
+setColor(9,0,"blue");
+setColor(9,9,"blue");
+setColor(3,4,"red");
+setColor(3,6,"red");
+setColor(5,3,"red");
+setColor(6,)
